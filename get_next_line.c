8c50c71e665605e7e7fa3ffc0bcdebc75ef58033
Istvan Willmott <istvan.willmott@gmail.com>
2022-03-25 18:33:30 +1030

    Second commit

--- get_next_line.c
+++ get_next_line.c
@@ -6,9 +6,23 @@
 /*   By: iwillmot <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/09 16:44:51 by iwillmot          #+#    #+#             */
-/*   Updated: 2022/03/11 12:58:47 by iwillmot         ###   ########.fr       */
+/*   Updated: 2022/03/25 18:32:51 by iwillmot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_readsave(int fd, char *save)
+{
+	char	*buff;
+	int		read_len;
+
+	buff = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buff)
+		return (NULL);
+	read_len = 1;
+	
+}
 
 char	*get_next_line(int fd)
 {
@@ -17,5 +31,5 @@ char	*get_next_line(int fd)
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (0);
-	
+	save = ft_readsave(fd, save);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,8 +6,16 @@
 /*   By: iwillmot <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/09 16:45:03 by iwillmot          #+#    #+#             */
-/*   Updated: 2022/03/09 16:45:05 by iwillmot         ###   ########.fr       */
+/*   Updated: 2022/03/25 18:32:54 by iwillmot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+
+#endif
