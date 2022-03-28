c81548f99be36e0ae3dba0b2b4d3e72c6addca02
Istvan Willmott <istvan.willmott@gmail.com>
2022-03-28 18:12:44 +1030

    Third commit

--- get_next_line.c
+++ get_next_line.c
@@ -6,30 +6,115 @@
 /*   By: iwillmot <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/09 16:44:51 by iwillmot          #+#    #+#             */
-/*   Updated: 2022/03/25 18:32:51 by iwillmot         ###   ########.fr       */
+/*   Updated: 2022/03/28 18:12:12 by iwillmot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*ft_readsave(int fd, char *save)
+char	*returnchar(char *save, char *final)
+{
+	char	*new;
+	int		i;
+
+	i = 0;
+	while ((save[i] != '\n') && (save[i] != '\0'))
+		i++;
+	new = malloc(i);
+	i = 0;
+	while ((save[i] != '\n') && (save[i] != '\0'))
+	{
+		new[i] = save[i];
+		i++;
+	}
+	if (save[i] == '\n')
+		new[i++] = '\n';
+	new[i] = '\0';
+	return (new);
+}
+
+int	search_n(char *buff)
+{
+	int ret;
+	int	i;
+
+	ret = 0;
+	i = 0;
+	while ((buff[i]) && (ret == 0))
+	{
+		if (buff[i] == '\n')
+			ret = 1;
+		i++;
+	}
+	return (ret);
+}
+char	*strjoin(char *dest, char *cpy)
+{
+	char	*new;
+	int		dlen;
+	int 	clen;
+
+	dlen = 0;
+	clen = 0;
+	while (dest[dlen])
+		dlen++;
+	while (cpy[clen])
+		clen++;
+	new = malloc(dlen + clen + 1);
+	dlen = 0;
+	clen = 0;
+	while (dest[dlen])
+	{
+		new[dlen] = dest[dlen];
+		dlen++;
+	}
+	while (cpy[clen])
+	{
+		new[dlen + clen] = cpy[clen];
+		clen++;
+	}
+	new[dlen + clen] = '\0';
+	free(dest);
+	return (new);
+}
+
+char	*readsave(int fd, char *save)
 {
 	char	*buff;
+	int		last;
 	int		read_len;
 
 	buff = malloc((BUFFER_SIZE + 1) * sizeof(char));
-	if (!buff)
-		return (NULL);
 	read_len = 1;
-	
+	last = 0;
+	while ((last == 0) && (read_len != 0))
+	{
+		read_len = read(fd, buff, BUFFER_SIZE);
+		if (read_len == -1)
+		{
+			free(buff);
+			return (NULL);
+		}
+		buff[read_len] = '\0';
+		last = search_n(buff);
+		save = strjoin(save, buff);
+	}
+	free(buff);
+	return (save);
 }
 
 char	*get_next_line(int fd)
 {
-	char		*line;
+	//char		*line;
 	static char	*save;
+	static char	*final;
 
+	save = (char *)malloc(1);
+	save[0] = '\0';
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (0);
-	save = ft_readsave(fd, save);
+	save = readsave(fd, save);
+	if (save)
+		free(save);
+	return (returnchar(save, final));
 }
